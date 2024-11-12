7bc0c848ceec1d8e221ea80df6ede0b28711bef9
Dike Wisdom <dikewisdom787@gmail.com>
2024-11-12 07:26:59 -0500

    Add DonatedItemsCount widget to display user donation statistics

---
+++ app/Filament/User/Widgets/DonatedItemsCount.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Filament\User\Widgets;
+
+use App\Models\Item;
+use Filament\Widgets\StatsOverviewWidget as BaseWidget;
+use Filament\Widgets\StatsOverviewWidget\Stat;
+use Illuminate\Support\Facades\Auth;
+
+class DonatedItemsCount extends BaseWidget
+{
+    protected function getStats(): array
+    {
+        return [
+            Stat::make('Total Donations', Item::where('user_id', Auth::user()->id)->count())
+                ->icon('heroicon-s-archive-box'),
+            Stat::make('Active Donations', Item::where([
+                'user_id' => Auth::user()->id,
+            ])->where('status', true)->count())
+                ->icon('heroicon-s-check-circle'),
+            Stat::make('Received Donations', Item::where([
+                'user_id' => Auth::user()->id,
+            ])->where('status', false)->appliedItems()->where('is_approved', true)->count())
+                ->icon('heroicon-s-x-circle'),
+        ];
+    }
+}
