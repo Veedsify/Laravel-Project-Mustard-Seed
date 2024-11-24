7b4660c3b84fb078520a6c66b390f04a9ab49d9e
Dike Wisdom <dikewisdom787@gmail.com>
2024-11-24 18:39:11 -0500

    Add dynamic tabs for user roles in ListUsers page

--- app/Filament/Admin/Resources/UserResource/Pages/ListUsers.php
+++ app/Filament/Admin/Resources/UserResource/Pages/ListUsers.php
@@ -3,13 +3,27 @@
 namespace App\Filament\Admin\Resources\UserResource\Pages;
 
 use App\Filament\Admin\Resources\UserResource;
+use App\Models\User;
 use Filament\Actions;
+use Filament\Resources\Components\Tab;
 use Filament\Resources\Pages\ListRecords;
 
 class ListUsers extends ListRecords
 {
     protected static string $resource = UserResource::class;
-
+    public function getTabs(): array
+    {
+        $roles = User::all()->pluck('role')->unique();
+        return array_merge([
+            'all' => Tab::make('All Users')            
+        ], collect($roles->mapWithKeys(function ($role) {
+            return [
+                strtoupper($role) => Tab::make($role)->query(function ($query) use ($role) {
+                    return $query->where('role', $role);
+                })
+            ];
+        }))->toArray());
+    }
     protected function getHeaderActions(): array
     {
         return [
