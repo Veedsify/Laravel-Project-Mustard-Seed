76321322cc1317df127e87420597cd63a1bf6739
Dike Wisdom <dikewisdom787@gmail.com>
2024-12-07 12:33:45 -0800

    Comment out conditional check for image status in gallery component

--- resources/views/livewire/gallery-component.blade.php
+++ resources/views/livewire/gallery-component.blade.php
@@ -2,11 +2,11 @@
     @if ($bottomGallery->count() >= 6)
         <div class="gallery-slider d-flex">
             @foreach ($bottomGallery as $image)
-                @if ($image->status == 1)
+                {{-- @if ($image->status == 1) --}}
                     <div class="gallery-img">
                         <img src="{{ asset('storage/' . $image->image_path) }}" alt="img" style="width: 100%;">
                     </div>
-                @endif
+                {{-- @endif --}}
             @endforeach
         </div>
     @endif  
