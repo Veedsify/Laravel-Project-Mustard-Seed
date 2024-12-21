efb805c93ffce0212bcfc236d49dcc3b01955a95
Dike Wisdom <dikewisdom787@gmail.com>
2024-12-21 14:16:35 -0800

    Replace video tags with YouTube iframes in user intro guide for enhanced video playback

--- resources/views/filament/user/widgets/user-intro-guide.blade.php
+++ resources/views/filament/user/widgets/user-intro-guide.blade.php
@@ -10,23 +10,25 @@
                 <div class="p-2">
                     <h2 class="text-lg font-semibold">Donators Guide</h2>
                 </div>
-                <video class="w-full h-full aspect-video object-cover" controls poster="{{ asset('images/guide-thumbnail.jpg') }}">
+                <iframe width="1920" height="1080" src="https://www.youtube.com/embed/Y8kU484gyj8?si=vcgyfUzznVXIFqnx" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+                {{-- <video class="w-full h-full aspect-video object-cover" controls poster="{{ asset('images/guide-thumbnail.jpg') }}">
                     <source
                         src="{{ asset('https://res.cloudinary.com/dymmepdu0/video/upload/f_auto:video,q_auto/olou63vmaco15e4tgvwn') }}"
                         type="video/mp4">
                     Your browser does not support the video tag.
-                </video>
+                </video> --}}
             </div>
             <div class="relative w-full rounded-lg overflow-hidden shadow-lg">
                 <div class="p-2">
                     <h2 class="text-lg font-semibold">Benefactors Guide</h2>
                 </div>
-                <video class="w-full h-full aspect-video object-cover" controls poster="{{ asset('images/guide-thumbnail.jpg') }}">
+                <iframe width="1920" height="1080" src="https://www.youtube.com/embed/osS-Q9cpitM?si=wCdKL4uxs5XcxMu1" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+                {{-- <video class="w-full h-full aspect-video object-cover" controls poster="{{ asset('images/guide-thumbnail.jpg') }}">
                     <source
                         src="{{ asset('https://res.cloudinary.com/dymmepdu0/video/upload/f_auto:video,q_auto/opqzurxydljmfhbt60fs') }}"
                         type="video/mp4">
                     Your browser does not support the video tag.
-                </video>
+                </video> --}}
             </div>
         </div>
     </x-filament::section>
