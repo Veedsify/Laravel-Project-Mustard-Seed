65ebc369b4a4c97f361204c84fb3065c62bc6d4e
Dike Wisdom <dikewisdom787@gmail.com>
2025-01-04 23:38:00 +0100

    Comment out user ID handling and authentication in LoginComponent

--- app/Livewire/LoginComponent.php
+++ app/Livewire/LoginComponent.php
@@ -8,21 +8,21 @@ use Livewire\Component;
 
 class LoginComponent extends Component
 {
-    public $id;
+    // public $id;
     public function mount()
     {
-        $userFromQuery = request()->query('id') ?? null;
-        if ($userFromQuery) {
-            $this->id = $userFromQuery;
-        } else {
-            $this->id = 1;
-        }
-        $userFromQuery = request()->query('id') ?? null;
-        $userFromQuery ? $this->id = $userFromQuery : $this->id = 1;
+        // $userFromQuery = request()->query('id') ?? null;
+        // if ($userFromQuery) {
+        //     $this->id = $userFromQuery;
+        // } else {
+        //     $this->id = 1;
+        // }
+        // $userFromQuery = request()->query('id') ?? null;
+        // $userFromQuery ? $this->id = $userFromQuery : $this->id = 1;
     }
     public function render()
     {
-        Auth::login(User::find($this->id));
+        // Auth::login(User::find($this->id));
         return view('livewire.login-component');
     }
 }
